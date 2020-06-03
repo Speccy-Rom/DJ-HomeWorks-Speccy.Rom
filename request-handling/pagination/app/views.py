36ba602ff1843f51ae861088063c1b82ee801318
ArtEpic <Speccy.Rom@gmail.com>
2020-06-03 19:15:45 +0300

    HomeWork-2(Task-1 v:1.0)

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
-
+import os
+import urllib.request
+import urllib.parse
+from urllib.parse import urlencode
+from app.settings import BUS_STATION_CSV
+import csv
+from django.core.paginator import Paginator
 
 def index(request):
     return redirect(reverse(bus_stations))
