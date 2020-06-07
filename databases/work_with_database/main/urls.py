15c1da0c609f9e03d33c6b4041ef59c806fad263
ArtEpic <Speccy.Rom@gmail.com>
2020-06-08 01:10:08 +0300

    Fix HomeWork-4

--- databases/work_with_database/main/urls.py
+++ databases/work_with_database/main/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
@@ -20,6 +21,6 @@ import phones.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', phones.views.show_catalog),
-    path('catalog/<slug:slug>/', phones.views.show_product),
+    path('catalog/', phones.views.show_catalog, name='catalog'),
+    url(r'^catalog/(?P<slug>[\w-]+)/$', phones.views.show_product, name='product'),
 ]
