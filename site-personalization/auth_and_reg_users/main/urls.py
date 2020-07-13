116bc034ec17fa30be2d6d4bc91d43b8ab4d87da
ArtEpic <Speccy.Rom@gmail.com>
2020-07-14 00:08:54 +0300

    Fix HomeWork-7 (Персонализация сайта) Speccy.Rom

--- site-personalization/auth_and_reg_users/main/urls.py
+++ site-personalization/auth_and_reg_users/main/urls.py
@@ -20,7 +20,9 @@ from django.urls import path
 import auth.views
 
 urlpatterns = [
-    path('', auth.views.home),
+    path('', auth.views.home, name='home'),
     path('admin/', admin.site.urls),
-    path('signup/', auth.views.signup),
+    path('signup/', auth.views.signup, name='signup'),
+    path('logout/', auth.views.logout_view, name='logout_view'),
+    path('login/', auth.views.login_view, name='login_view'),
 ]
--- site-personalization/paid_content/articles/views.py
+++ site-personalization/paid_content/articles/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
-
+from .models import Article, Profile
 
 def show_articles(request):
+    art = Article.objects.all()
     return render(
         request,
-        'articles.html'
+        'articles.html',
+        {'art': art}
     )
 
 
 def show_article(request, id):
+    obj = Article.objects.get(id=id)
+    user = request.user
+    prof = Profile.objects.get(user=user).has_subcription
+    if obj.pay_article and not prof:
+        context = {'title': obj.title, 'limited': 'Статья платная, пожалуйста, подпишитесь'}
+    else:
+        context = {'title': obj.title, 'body': obj.text}
     return render(
         request,
-        'article.html'
+        'article.html',
+        context
     )
